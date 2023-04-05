178edc29bcf28373ec3ce8d61cac7b09e23dc7a2
NethrajM <123164428+NethrajM@users.noreply.github.com>
2023-04-05 18:04:26 +0530

    Add files via upload

---
+++ Univariate_Analysis.py
@@ -0,0 +1,12 @@
+class Univariate():
+    def quanQual(dataset):
+        quan=[]
+        qual=[]
+        for columnName in dataset.columns:
+            if(dataset[columnName].dtype=='O'):
+                qual.append(columnName)
+            else:
+                quan.append(columnName)
+        return quan,qual      
+                
+            
